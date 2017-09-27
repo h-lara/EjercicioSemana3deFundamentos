922fc4a3dc12256a6c011a0ead047be12b18020d
Hernan <hernan.lara@usach.cl>
2017-09-26 21:48:45 -0300

    Initial commit

---
+++ app/src/main/java/hernanlara/ejerciciocoursera/com/ejerciciosemana3defundamentos/MainActivity.java
@@ -0,0 +1,13 @@
+package hernanlara.ejerciciocoursera.com.ejerciciosemana3defundamentos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
